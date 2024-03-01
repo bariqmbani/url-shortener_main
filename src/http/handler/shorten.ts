582720241a69a66a99d26fd17d5a7e09a1e51ce8
Bariq Mbani <bariq.m70@gmail.com>
2024-03-01 19:38:11 +0700

    fix: accidentally deleted function call

--- src/http/handler/shorten.ts
+++ src/http/handler/shorten.ts
@@ -6,6 +6,8 @@ export const shorten = async (req: Request, res: Response) => {
   const url = req.body && req.body.url ? req.body.url : "https://youtube.com";
   const client = req.body && req.body.client ? req.body.client : "test-client";
 
+  urlKeyProducer(client);
+
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
